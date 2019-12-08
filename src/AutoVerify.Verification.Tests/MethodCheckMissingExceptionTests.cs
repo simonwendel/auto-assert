84e221ecdfe6deafc4b6d0ad66bba5ef3219104e
Simon Wendel <mail@simonwendel.se>
2019-12-08 17:29:18 +0100

    feat: add exception to throw when method check is missing

---
+++ src/AutoVerify.Verification.Tests/MethodCheckMissingExceptionTests.cs
@@ -0,0 +1,121 @@
+﻿namespace AutoVerify.Verification.Tests
+{
+    using System;
+    using AutoFixture;
+    using FluentAssertions;
+    using Xunit;
+
+    public class MethodCheckMissingExceptionTests
+    {
+        private readonly string methodName;
+        private readonly string otherMethodName;
+        private readonly string paramName;
+        private readonly string otherParamName;
+        private readonly MethodCheckMissingException sut;
+
+        public MethodCheckMissingExceptionTests()
+        {
+            var fixture = new Fixture();
+
+            methodName = fixture.Create<string>();
+            otherMethodName = fixture.Create<string>();
+
+            paramName = fixture.Create<string>();
+            otherParamName = fixture.Create<string>();
+
+            sut = new MethodCheckMissingException(methodName, paramName);
+        }
+
+        [Fact]
+        public void Constructor_GivenNullMethodName_DoesNotThrow()
+        {
+            Action constructing = () => new MethodCheckMissingException(null, paramName);
+            constructing.Should().NotThrow();
+        }
+
+        [Fact]
+        public void Constructor_GivenNullParamName_DoesNotThrow()
+        {
+            Action constructing = () => new MethodCheckMissingException(methodName, null);
+            constructing.Should().NotThrow();
+        }
+
+        [Fact]
+        public void Constructor_GivenMethodName_SetsMethodNameProperty()
+        {
+            sut.MethodName.Should().Be(methodName);
+        }
+
+        [Fact]
+        public void Constructor_GivenParamName_SetsParamNameProperty()
+        {
+            sut.ParamName.Should().Be(paramName);
+        }
+
+        [Fact]
+        public void Equals_GivenNull_ReturnsFalse()
+        {
+            sut.Equals(null).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Equals_GivenWrongType_ReturnsFalse()
+        {
+            sut.Equals(new object()).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Equals_GivenOtherMethodName_ReturnsFalse()
+        {
+            var otherException = new MethodCheckMissingException(otherMethodName, paramName);
+            sut.Equals(otherException).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Equals_GivenOtherParamName_ReturnsFalse()
+        {
+            var otherException = new MethodCheckMissingException(methodName, otherParamName);
+            sut.Equals(otherException).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Equals_GivenSameMethodNameAndParamName_ReturnsTrue()
+        {
+            var otherException = new MethodCheckMissingException(methodName, paramName);
+            sut.Equals(otherException).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Equals_GivenSameObject_ReturnsTrue()
+        {
+            sut.Equals(sut).Should().BeTrue();
+        }
+
+        [Fact]
+        public void GetHashCode_WhenMethodNameDiffers_ReturnsDifferentHashCode()
+        {
+            var otherException = new MethodCheckMissingException(otherMethodName, paramName);
+            sut.GetHashCode().Should().NotBe(otherException.GetHashCode());
+        }
+
+        [Fact]
+        public void GetHashCode_WhenParamNameDiffers_ReturnsDifferentHashCode()
+        {
+            var otherException = new MethodCheckMissingException(methodName, otherParamName);
+            sut.GetHashCode().Should().NotBe(otherException.GetHashCode());
+        }
+
+        [Fact]
+        public void GetHashCode_WhenSameMethodNameAndParamName_ReturnsSameHashCode()
+        {
+            var otherException = new MethodCheckMissingException(methodName, paramName);
+            sut.GetHashCode().Should().Be(otherException.GetHashCode());
+        }
+
+        [Fact]
+        public void GetHashCode_WhenCalledOnSameObject_ReturnsSameHashCode()
+        {
+            sut.GetHashCode().Should().Be(sut.GetHashCode());
+        }
+    }
+}
---
+++ src/AutoVerify.Verification/MethodCheckMissingException.cs
@@ -0,0 +1,30 @@
+﻿namespace AutoVerify.Verification
+{
+    using System.Collections.Generic;
+
+    public class MethodCheckMissingException : CheckMissingException
+    {
+        public MethodCheckMissingException(string methodName, string paramName)
+        {
+            MethodName = methodName;
+            ParamName = paramName;
+        }
+
+        public string MethodName { get; }
+
+        public string ParamName { get; }
+
+        public override bool Equals(object obj)
+            => obj is MethodCheckMissingException other
+                && MethodName == other.MethodName
+                && ParamName == other.ParamName;
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1288608676;
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(MethodName);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(ParamName);
+            return hashCode;
+        }
+    }
+}
