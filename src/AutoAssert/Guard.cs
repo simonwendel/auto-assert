8784d8f8946a5b784b8f67537410112cbb3e7925
Simon Wendel <mail@simonwendel.se>
2019-12-10 17:59:58 +0100

    feat: add a Guard statement class

---
+++ src/AutoAssert/Guard.cs
@@ -0,0 +1,22 @@
+﻿namespace AutoAssert
+{
+    using System;
+    using System.Diagnostics;
+
+    internal static class Guard
+    {
+        [DebuggerHidden]
+        public static void AgainstNull([ValidatedNotNull] object parameter, string parameterName)
+        {
+            if (parameter is null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
+
+        [AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = true)]
+        internal sealed class ValidatedNotNullAttribute : Attribute
+        {
+        }
+    }
+}
